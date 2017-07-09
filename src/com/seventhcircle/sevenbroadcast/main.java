4215983f2ed10822694a855904749ee52aca1c7e
SeventhCircle <unconfigured@null.null.org>
2017-07-09 16:57:44 -0400

    V1.0

--- src/com/seventhcircle/sevenbroadcast/main.java
+++ src/com/seventhcircle/sevenbroadcast/main.java
@@ -4,68 +4,36 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
 	
 	public void onEnable()
 	{
+		// Tells the Console that the plugin has been loaded
 		getLogger().info("Plugin Enabled!");
 	}
-	
-	//String[] messages = {"Message 1", "Message 2", "Message 3", "Message 4"};
+	// The Prefix used before any broadcast messages
 	String prefix = ChatColor.RED + "[" + ChatColor.GRAY + "Admin Broadcast" + ChatColor.RED + "] " + ChatColor.LIGHT_PURPLE;
-	
+
+	// The Broadcast Command
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("broadcast")){
-			Player player = (Player) sender;
-			if(player.isOp()){
-			
-				
-				
-				
-				
-				
-				/** if(args[0].equalsIgnoreCase("1")){
-					Bukkit.broadcastMessage(prefix + messages[0]);
-				}
-				else if(args[0].equalsIgnoreCase("2")){
-					Bukkit.broadcastMessage(prefix + messages[1]);
-				}
-				else if(args[0].equalsIgnoreCase("3")){
-					Bukkit.broadcastMessage(prefix + messages[2]);
-				}
-				else if(args[0].equalsIgnoreCase("4")){
-					Bukkit.broadcastMessage(prefix + messages[3]);
-				} **/
+				String message = "";
+				for(String part : args){
+					if(message != "") message += " ";
+				message += part;
+				}	
+					if(sender.isOp()){
+						Bukkit.broadcastMessage(prefix + message);
+						return true;
+					}
+					else sender.sendMessage(ChatColor.DARK_RED + "You lack permission to do this");
+					return true;			
 			}
+				return false;
 		}
-		return false;
 	}
-}
+
 	
-/**	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if(cmd.getName().equalsIgnoreCase("ab")){
-			Player player = (Player) sender;
-			if(player.isOp()){
-				if(args[0].equalsIgnoreCase("1")){
-					Bukkit.broadcastMessage(ChatColor.RED + "[" + ChatColor.GRAY + "Admin Broadcast" + ChatColor.RED + "]" + ChatColor.GREEN + "Message 1");
-				}
-			
-				else if(args[0].equalsIgnoreCase("2")){
-					Bukkit.broadcastMessage(ChatColor.RED + "[" + ChatColor.GRAY + "Admin Broadcast" + ChatColor.RED + "]" + ChatColor.GREEN + "Message 2");
-			}
-				
-				else if(args[0].equalsIgnoreCase("3")){
-					Bukkit.broadcastMessage(ChatColor.RED + "[" + ChatColor.GRAY + "Admin Broadcast" + ChatColor.RED + "]" + ChatColor.GREEN + "Message 3");
-				}
-			
-				else if(args[0].equalsIgnoreCase("4")){
-					Bukkit.broadcastMessage(ChatColor.RED + "[" + ChatColor.GRAY + "Admin Broadcast" + ChatColor.RED + "]" + ChatColor.GREEN + "Message 4");
-				}
-			}
-		}
-		return false;
-	}
-} **/
+
